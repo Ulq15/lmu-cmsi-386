ca2d7f4c32a5e6e936f50a988f0f551cf0e2e42e
H. H. Hassan <Ulq1517@users.noreply.github.com>
2020-11-29 16:43:11 +0200

    implmented Queue
    need to make Queue pass test_no_copies() and test_moves(), currently Queue fails those 2 but pass the rest

--- homework5/exercises.h
+++ homework5/exercises.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <list>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -32,3 +33,64 @@ auto sorted_word_counts(list<string> words) {
 	sort(v.begin(), v.end(), [](auto x, auto y) {return x.second > y.second; });
 	return v;
 };
+
+template <class T>
+class Queue {
+private:
+	struct Node {
+		T data;
+		Node* next;
+		Node(T data, Node* next) :data(data), next(next) {}
+	};
+	Node* head;
+	Node* tail;
+	int cur_size;
+public:
+	Queue() : head(nullptr), tail(nullptr) {}
+	~Queue() {
+		while (head != nullptr) {
+			Node* prevHead = head;
+			head = head->next;
+			delete prevHead;
+		}
+	}
+	void enqueue(T item) {
+		if (isEmpty()) {
+			head = new Node(item, head);
+			tail = head;
+		}
+		else {
+			tail->next = new Node(item, nullptr);
+			tail = tail->next;
+		}
+		cur_size++;
+	}
+	T dequeue() {
+		if (isEmpty()) {
+			throw underflow_error("Cannot dequeue an empty Queue");
+		}
+		Node* node_to_dequeue = head;
+		T dequeued_data = head->data;
+		head = head->next;
+		delete node_to_dequeue;
+		cur_size--;
+		return dequeued_data;
+	}
+	int get_size() {
+		return this->cur_size;
+	}
+	bool isEmpty() {
+		return get_size() == 0;
+	}
+	ostream& operator<<(ostream& os) {
+		Node* cur_Node = head;
+		os << '{';
+		while (cur_Node != tail) {
+			os << cur_Node->data << ", ";
+			cur_Node = cur_Node->next;
+		}
+		os << cur_Node->data << '}';
+		return os;
+	}
+};
+
--- homework5/exercises_test.cpp
+++ homework5/exercises_test.cpp
@@ -29,7 +29,7 @@ void test_sorted_counts() {
         assert(sorted_word_counts(words) == counts);
     }
 }
-/*
+
 void test_empty_queue_has_zero_size() {
     Queue<string> q;
     assert(q.get_size() == 0);
@@ -95,16 +95,14 @@ void test_dequeue_from_empty_queue_throws_underflow_error() {
         assert(false);
     }
 }
-*/
+
 int main() {
     test_say();
     test_sorted_counts();
-    /*test_empty_queue_has_zero_size();
+    test_empty_queue_has_zero_size();
     test_some_insertions_and_deletions();
     test_dequeue_from_empty_queue_throws_underflow_error();
-    test_no_copies();
-    test_moves();
-    */
+    //test_no_copies();
+    //test_moves();
     cout << "All tests passed\n";
-    
 }
